47607f9c061bba7db2effd26fff81c3810d845fc
jangir-ritik <115213651+jangir-ritik@users.noreply.github.com>
2023-03-11 00:40:50 +0530

    MAJOR: added global theme to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Routes, Route, Navigate, useSearchParams, BrowserRouter, useLocation } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
 
 import IssueList from './IssueList.jsx';
 import IssueEdit from './IssueEdit.jsx';
+import PrimarySearchAppBar from './Components/PrimarySearchAppBar.jsx';
+import { lightTheme, darkTheme } from './Components/ThemeProvider.jsx';
 
 const NoMatch = () => <p>Page Not Found</p>;
 
 const App = () => { 
   const [searchParams] = useSearchParams()
+  const [themeMode, setThemeMode] = useState('light');
+
   return (
-    <div>
-      <div className="header">
-        <h1>Issue Tracker</h1>
+    <ThemeProvider theme={themeMode === 'light' ? lightTheme : darkTheme}>
+      <div>
+        {/* <div className="header"> */}
+          {/* <h1>Issue Tracker</h1> */}
+          <PrimarySearchAppBar themeMode={themeMode} setThemeMode={setThemeMode}/>
+        {/* </div> */}
+        <div className="contents">
+          <Routes>
+            <Route path="/" element={<Navigate status={searchParams} to="/issues" />} />
+            <Route path="/issues" element={<IssueList status={searchParams} />} />
+            <Route path="/issues/:id" element={<IssueEdit />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+        </div>
+        <div className="footer">
+          Made with love and confusion thanks to react router dom.
+        </div>
       </div>
-      <div className="contents">
-        <Routes>
-          <Route path="/" element={<Navigate status={searchParams} to="/issues" />} />
-          <Route path="/issues" element={<IssueList status={searchParams} />} />
-          <Route path="/issues/:id" element={<IssueEdit />} />
-          <Route path="*" element={<NoMatch />} />
-        </Routes>
-      </div>
-      <div className="footer">
-        Made with love and confusion thanks to react router dom.
-      </div>
-    </div>
+    </ThemeProvider>
 );
 }
 
---
+++ src/Components/ThemeProvider.jsx
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+export const lightTheme = createTheme({
+    palette: {
+      mode: 'light',
+      // define your light mode palette here
+    },
+  });
+  
+  export const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      // define your dark mode palette here
+    },
+  });
