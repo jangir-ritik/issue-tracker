b9696f943935e55d28781e4284a28f368d972116
jangir-ritik <115213651+jangir-ritik@users.noreply.github.com>
2023-03-16 21:22:45 +0530

    MAJOR: added bg color to the app body

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { Routes, Route, Navigate, useSearchParams, BrowserRouter, useLocation } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import { Box } from '@mui/material'
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid'
-import Item from '@mui/material/Grid'
-import Layout from './Components/Layout.jsx';
 
 import IssueList from './IssueList.jsx';
 import IssueEdit from './IssueEdit.jsx';
@@ -15,47 +11,39 @@ import { lightTheme, darkTheme } from './Components/ThemeProvider.jsx';
 
 const NoMatch = () => <p>Page Not Found</p>;
 
-const App = () => { 
+const App = () => {
   const [searchParams] = useSearchParams()
   const [themeMode, setThemeMode] = useState('light');
+  const boxStyle = { 
+    backgroundColor: themeMode === 'light' ? lightTheme.palette.background.default : darkTheme.palette.background.default,
+    color: themeMode === 'light' ? lightTheme.palette.text.primary : darkTheme.palette.text.primary,
+}
 
   return (
     <ThemeProvider theme={themeMode === 'light' ? lightTheme : darkTheme}>
-      <div>
-        <PrimarySearchAppBar themeMode={themeMode} setThemeMode={setThemeMode}/>
-        {/* <br /> */}
-        {/* <div className="contents"> */}
-        <Box sx={{ marginTop: 2 }} component="main">
-            <Routes>
-              <Route path="/" element={<Navigate status={searchParams} to="/issues" />} />
-              <Route path="/issues" element={<IssueList status={searchParams} />} />
-              <Route path="/issues/:id" element={<IssueEdit />} />
-              <Route path="*" element={<NoMatch />} />
-            </Routes>
-        {/* </div> */}
+      <Box sx={boxStyle} component="div">
+        <PrimarySearchAppBar themeMode={themeMode} setThemeMode={setThemeMode} />
+        <Box sx={{ marginTop: 2, height: '100vh' }} component="main">
+          <Routes>
+            <Route path="/" element={<Navigate status={searchParams} to="/issues" />} />
+            <Route path="/issues" element={<IssueList status={searchParams} />} />
+            <Route path="/issues/:id" element={<IssueEdit />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+          {/* </div> */}
         </Box>
         <div className="footer">
           Made with love and confusion thanks to react router dom.
         </div>
-        {/* <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}
-          // style={{ backgroundColor: themeMode.palette.background.default, color: themeMode.palette.text.primary }}
-        >
-          {Array.from(Array(6)).map((_, index) => (
-            <Grid item xs={2} sm={4} md={4} key={index}>
-              This is item 1
-            </Grid>
-          ))}
-        </Grid> */}
-        {/* <Layout /> */}
-      </div>
+      </Box>
     </ThemeProvider>
-);
+  );
 }
 
 createRoot(document.getElementById('contents')).render(
-<BrowserRouter>
-  <App />
-</BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 if (module.hot) {
